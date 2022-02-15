5b0cd238a3fce7001c5e45fccbeb2d663f6073ab
VIK2001 <99675380+VIK2001@users.noreply.github.com>
2022-02-15 15:34:16 +0530

    Update RX.py

--- RX.py
+++ RX.py
@@ -10,6 +10,7 @@ from base64 import b64decode,b64encode
 from datetime import date
 
 expirydate = datetime.date(2021, 12, 27)
+expirydate = datetime.date(2021, 12, 31)
 #expirydate = datetime.date(2021, 12, 31)
 today=date.today()
 def hero():
